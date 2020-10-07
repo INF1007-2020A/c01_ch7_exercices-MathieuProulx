f953e64450800756da9d2b80a7045108607feffe
Mathieu Proulx <mathieu.proulx@polymtl.ca>
2020-10-07 15:50:46 -0400

    1. fait, 3. non

--- exercice.py
+++ exercice.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# TODO: Importez vos modules ici
+import math
+import turtle
 
+def ellipsoide(a=1, b=1, c=1, v=1):
+    volume =  (4/3) * a * b * c * math.pi
+    masse = volume * v
 
+    return volume, masse
 
-# TODO: Définissez vos fonction ici
+def tree():
+    turtle.forward(200*(0.90**n))
+    turtle.right(45/n+1)
+
 
 
 if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
+    print(ellipsoide(4,5,1,0.75))
 
     pass
