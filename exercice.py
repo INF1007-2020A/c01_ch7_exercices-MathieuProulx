ab99fd58297d31b62446854de3fce91f9ceb0b68
Mathieu Proulx <mathieu.proulx@polymtl.ca>
2020-10-19 14:40:51 -0400

    sort fais

--- exercice.py
+++ exercice.py
@@ -3,6 +3,7 @@
 
 import math
 import turtle
+import ch6
 
 def ellipsoide(a=1, b=1, c=1, v=1):
     volume =  (4/3) * a * b * c * math.pi
@@ -10,13 +11,9 @@ def ellipsoide(a=1, b=1, c=1, v=1):
 
     return volume, masse
 
-def tree():
-    turtle.forward(200*(0.90**n))
-    turtle.right(45/n+1)
-
-
+def sort(sequence: str) -> tuple:
+    return sorted(histogram(sequence).items(), key=lambda x: x[1])[-1]
 
 if __name__ == '__main__':
     print(ellipsoide(4,5,1,0.75))
-
     pass
